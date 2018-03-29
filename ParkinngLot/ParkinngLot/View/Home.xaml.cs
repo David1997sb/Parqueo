8338e08a3cedbb58b8b61fd232441e1cd1544217
colochos <adrian0897@outlook.es>
2018-03-29 17:40:17 -0600

    Añadiendo interfaces
    Se agregan interfaces principales

---
+++ ParkinngLot/ParkinngLot/View/Home.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ParkingLot.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Home : ContentPage
+	{
+		public Home ()
+		{
+			InitializeComponent ();
+            bntLogin.Clicked += BntLogin_Clicked;
+            btnSignIn.Clicked += BtnSignIn_Clicked;
+        }
+
+        private void BtnSignIn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SignIn());
+
+        }
+
+        private void BntLogin_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Login());
+        }
+    }
+}
